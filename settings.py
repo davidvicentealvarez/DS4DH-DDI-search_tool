1d41fca9ab5a79729267aa39a9dc3a600b395b84
davidvicentealvarez <34418704+davidvicentealvarez@users.noreply.github.com>
2023-08-03 14:24:06 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ ES_HOST = "https://localhost"
 ES_PORT = "9800"
 ES_CERT_PATH = "../elasticsearch-8.5.0/config/certs/http_ca.crt"
 ES_USER = "elastic"
-ES_PWD = "UA4=3fUJzFqTdYY53*0e"
+ES_PWD = "UA4=3fUJzFqTdYY53*0e" #this passw is not in use anymore 
 DRIVE_PATH = "..\\drive\\HFPS Questionnaires"
 LOG_LEVEL="DEBUG"
