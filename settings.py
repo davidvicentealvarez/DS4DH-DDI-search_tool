fbbe5c8715f87760d2b6bce9c4153061c704d915
davidvicentealvarez <34418704+davidvicentealvarez@users.noreply.github.com>
2024-11-21 14:05:02 +0100

    Update with Docker

--- settings.py
+++
@@ -1,9 +0,0 @@
-ES_INDEX_DOC_MAME = "disabilities_hfps"
-ES_INDEX_CHUNK_MAME = "disabilities_chunks_hfps"
-ES_HOST = "https://localhost"
-ES_PORT = "9800"
-ES_CERT_PATH = "../elasticsearch-8.5.0/config/certs/http_ca.crt"
-ES_USER = "elastic"
-ES_PWD = "UA4=3fUJzFqTdYY53*0e" #this passw is not in use anymore 
-DRIVE_PATH = "..\\drive\\HFPS Questionnaires"
-LOG_LEVEL="DEBUG"
